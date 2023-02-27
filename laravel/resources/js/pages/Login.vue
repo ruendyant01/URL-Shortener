11269ff34fc6d462b5a95f056958e962a5f7d182
ruendyant01 <ruendyant.mayraclle@gmail.com>
2023-02-27 22:02:45 +0700

    style: Register Page

--- laravel/resources/js/pages/Login.vue
+++ laravel/resources/js/pages/Login.vue
@@ -2,8 +2,8 @@
     <div class="flex flex-col items-center space-y-8 mt-2">
         <h1 class="text-4xl">Login</h1>
         <form class="flex flex-col space-y-4 [&_input]:py-1 [&_input]:px-3 [&_input]:border-b [&_input]:border-slate-400">
-            <input type="email" name="email" placeholder="Email">
-            <input type="password" name="password" placeholder="Password">
+            <input type="email" name="email" placeholder="Email" v-model="email">
+            <input type="password" name="password" placeholder="Password" v-model="password">
             <button type="submit" class="border-b border-slate-400 py-2 px-4 rounded-lg">Log in</button>
         </form>
     </div>
@@ -11,6 +11,14 @@
 
 <script>
 export default {
+    data() {
+        return {
+            form: {
+                email: "",
+                password: ""
+            }
+        }
+    }
     
 }
 </script>
